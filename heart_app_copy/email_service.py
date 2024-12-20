537675cfcdb9eb16276a395e3fdf3ad67568bf48
erich <ericheinrich123@gmail.com>
2024-12-19 22:24:36 -0800

    Implement dotenv on email service for secure credentials

--- heart_app_copy/email_service.py
+++ heart_app_copy/email_service.py
@@ -1,13 +1,16 @@
 import smtplib
 from email.mime.text import MIMEText
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
 # Define the send_mail function
 def send_mail(to_email, subject, body):
     smtp_server = "smtp.gmail.com"
     smtp_port = 587
-    sender_email = "email.service.54321@gmail.com"  # Replace with your Gmail address
-    sender_password = "xwilthgbtupphcvt"  # Replace with your Gmail App Password
+    sender_email = os.getenv("EMAIL_ADDRESS")  # Replace with your Gmail address
+    sender_password = os.getenv("EMAIL_PASSWORD")  # Replace with your Gmail App Password
 
     try:
         # Create the email content
